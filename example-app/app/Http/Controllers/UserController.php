7b97e32a18e997ad4ca9e842e75c5dd5e7df9356
TiestoTool <9019545@student.zadkine.nl>
2023-12-22 21:23:03 +0100

    Bugfix + database

--- example-app/app/Http/Controllers/UserController.php
+++ example-app/app/Http/Controllers/UserController.php
@@ -1,17 +1,22 @@
 <?php
+ 
+ namespace App\Http\Controllers;
+ 
+ use App\Models\User; 
+ use Illuminate\Http\Request;
+  
+ class UserController extends Controller
+ {
+     public function register(Request $request){
+         $incomingFields = $request->validate([
+             'name' => ['required', 'min:3' ,'max:10'],
+             'email' => ['required', 'email'],
+             'password' =>['required', 'min:8', 'max:200']
+         ]);
+         
+         $incomingFields['password'] = bcrypt($incomingFields['password']);
+         User::create($incomingFields);
 
-namespace App\Http\Controllers;
-
-use Illuminate\http\Request;
-
-class UserController extends Controller
-{
-    public function register(Request $request){
-        $incomingFields = $request->validate([
-            'name' => ['required', 'min:3' ,'max:10'],
-            'email' => ['required', 'email'],
-            'password' =>['required', 'min:8', 'max:200']
-        ]);
-        return 'Hello form our controller';
-    }
-}
+         return 'Hello form our controller';
+     }
+ }
