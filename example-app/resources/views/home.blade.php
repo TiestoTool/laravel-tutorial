98d9e2ca424bd6790238f723c03f075595a3c0fc
TiestoTool <9019545@student.zadkine.nl>
2023-12-22 21:36:44 +0100

    Log in

--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -6,6 +6,10 @@
     <title>Document</title>
 </head>
 <body>
+
+    @auth
+        <p>You are logged in.</p>
+    @else
     <div style="border: 3px solid black;">
         <h2>Register</h2>
         <form action="/register" method="POST">
@@ -16,5 +20,7 @@
             <button>Register</button>
         </form>
     </div>
+    @endauth
+    
 </body>
 </html>
